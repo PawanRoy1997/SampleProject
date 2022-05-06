2f6f1d57ada356d159cea03cf82bd7610bcaec7e
PawanRoy1997 <pawanroy0066@gmail.com>
2022-05-06 12:14:39 +0530

    Timestamp in human readable format

--- app/src/main/java/com/example/sampleproject/adapters/LogViewHolder.kt
+++ app/src/main/java/com/example/sampleproject/adapters/LogViewHolder.kt
@@ -4,13 +4,18 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sampleproject.databinding.LogItemBinding
 import com.example.sampleproject.db.Log
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var binding = LogItemBinding.bind(itemView)
+    private val sdf = SimpleDateFormat("dd MM yyyy HH:mm", Locale.ENGLISH).apply {
+        timeZone = TimeZone.getTimeZone("Asia/Kolkata")
+    }
 
     fun bind(data: Log) {
         binding.logMessage.text = data.message
-        binding.logTimeStamp.text = data.timestamp.toString()
+        binding.logTimeStamp.text = sdf.format(data.timestamp)
         binding.logCount.text = data.countValue.toString()
     }
 }
